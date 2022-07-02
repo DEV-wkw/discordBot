2232bbe4d8bb2eb5e4a16a63bb6e55978d9ff13d
DEV-wkw <53428658+DEV-wkw@users.noreply.github.com>
2022-07-02 23:50:51 +0800

    feat(user): user xp and level system

--- cogs/chat.py
+++ cogs/chat.py
@@ -2,11 +2,16 @@ import discord
 from discord.ext import commands
 
 class chat(commands.Cog):
-    
+
+  def __init__(self, bot = None):
+  self.bot = bot
+
   @commands.command()
   async def clear(self, ctx):
     await ctx.channel.purge(limit = 20, check = lambda x: not x.embeds)
     await ctx.send('```20 messages has been deleted.```', delete_after=3)
+    user = self.bot.get_cog('database')
+    await user.addXp(ctx, 3)
 
   @commands.command()
   async def hide(self, ctx, message):
--- main.py
+++ main.py
@@ -8,6 +8,15 @@ c = commands.Bot(command_prefix = '')
 @c.event
 async def on_ready():
   print('program started')
+
+@c.event
+async def on_message(message):
+  if(int(message.author.id) != int(os.getenv('bot_id_token'))):
+    ctx = await c.get_context(message)
+    user = c.get_cog('user')
+    await user.addNewUser(ctx)
+    await user.addXp(ctx, 3)
+    await c.process_commands(message)
     
 for file in os.listdir('./cogs'):
   if file.endswith('.py'):
